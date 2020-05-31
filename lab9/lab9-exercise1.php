654969ce9e3f28ba9aa26e08724b081d8389ca8f
chenmin <19302010057@fudan.edu.cn>
2020-06-01 00:29:38 +0800

    lab9

---
+++ lab9/lab9-exercise1.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <title>Exercise 1</title>
+</head>
+<body>
+<h1>Regular HTML section (outside &lt;?php ... ?&gt; tags)</h1>
+<p>You can type regular HTML here and it will show up</p>
+
+<h1>PHP section (inside &lt;?php ... ?&gt; tags)</h1>
+<?php
+//this is a php comment IN tags (will not appear)
+$remaining = 365 - date("z");
+if(date("L")) {
+    $remain = $remaining +  1;
+    echo "There are " . $remain . " days left in the year";
+}
+else
+    echo "There are " . $remaining . " days left in the year";
+echo "<br>";
+
+?>
+</body>
+</html>
